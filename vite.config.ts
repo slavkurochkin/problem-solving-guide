7074f3338007382e72b0e328df20a7968b6bff19
slavkurochkin <kurochkinslav@gmail.com>
2025-08-26 00:50:03 -0400

    Set base path for deployment to GitHub Pages in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/problem-solving-guide/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
